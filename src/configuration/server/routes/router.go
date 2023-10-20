bf837a8fc59ffce5118453ce92f3f3c5d872fe3c
kaikeventura <kaike.gnr@hotmail.com>
2023-10-19 21:12:36 -0300

    Criado rora para planos de testes com http

--- src/configuration/server/routes/router.go
+++ src/configuration/server/routes/router.go
@@ -2,8 +2,17 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/kaikeventura/cat-runner/src/app/controller"
 )
 
 func RouterConfiguration(router *gin.Engine) *gin.Engine {
+	v1 := router.Group("v1")
+	{
+		runner := v1.Group("runner")
+		{
+			runner.POST("/http", controller.RunHttp)
+		}
+	}
+
 	return router
 }
