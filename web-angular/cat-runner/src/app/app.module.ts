80b68bdceee4752203153242476f1fdbd9d557b2
kaikeventura <kaike.gnr@hotmail.com>
2023-10-28 01:21:02 -0300

    Criado estrutura de cards para listar as estratégia de testes existentes

--- web-angular/cat-runner/src/app/app.module.ts
+++ web-angular/cat-runner/src/app/app.module.ts
@@ -7,18 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { StrategiesListComponent } from './views/home/strategies-list/strategies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    StrategiesListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
